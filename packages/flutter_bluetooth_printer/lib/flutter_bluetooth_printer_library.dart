35be8baeb54c8ac04f4b6d224dc4bd2d37264c09
Eka Setiawan Saputra <mr.poetra22@gmail.com>
2022-07-09 11:08:00 +0700

    refactor: refactor

--- packages/flutter_bluetooth_printer/lib/flutter_bluetooth_printer_library.dart
+++ packages/flutter_bluetooth_printer/lib/flutter_bluetooth_printer_library.dart
@@ -3,7 +3,6 @@ library flutter_bluetooth_printer;
 import 'dart:async';
 import 'dart:typed_data';
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
@@ -13,5 +12,6 @@ import 'package:image/image.dart' as img;
 import 'package:pdfx/pdfx.dart' as rd;
 
 part 'src/flutter_bluetooth_printer_impl.dart';
-part 'src/widgets/bluetooth_device_selector.dart';
+part 'src/method_channel/method_channel_bluetooth_printer.dart';
 part 'src/widgets/receipt.dart';
+part 'src/widgets/bluetooth_device_selector.dart';
--- packages/flutter_bluetooth_printer/lib/src/flutter_bluetooth_printer_impl.dart
+++ packages/flutter_bluetooth_printer/lib/src/flutter_bluetooth_printer_impl.dart
@@ -1,12 +1,10 @@
 part of flutter_bluetooth_printer;
 
-class FlutterBluetoothPrinterPlugin {
+class FlutterBluetoothPrinter {
   static void registerWith() {
     FlutterBluetoothPrinterPlatform.instance = _MethodChannelBluetoothPrinter();
   }
-}
 
-class FlutterBluetoothPrinter {
   static Stream<List<BluetoothDevice>> _discovery() async* {
     final result = <BluetoothDevice>[];
     await for (final device
@@ -122,69 +120,5 @@ class FlutterBluetoothPrinter {
       return selected;
     }
     return null;
-  }
-}
-
-class _MethodChannelBluetoothPrinter extends FlutterBluetoothPrinterPlatform {
-  final channel = const MethodChannel('maseka.dev/flutter_bluetooth_printer');
-  final discoveryChannel =
-      const EventChannel('maseka.dev/flutter_bluetooth_printer/discovery');
-
-  ProgressCallback? _progressCallback;
-
-  bool _isInitialized = false;
-  void _init() {
-    if (_isInitialized) return;
-    _isInitialized = true;
-    channel.setMethodCallHandler((call) async {
-      switch (call.method) {
-        case 'didUpdateState':
-          final index = call.arguments as int;
-          connectionStateNotifier.value =
-              BluetoothConnectionState.values[index];
-          break;
-
-        case 'onPrintingProgress':
-          final total = call.arguments['total'] as int;
-          final progress = call.arguments['progress'] as int;
-          _progressCallback?.call(total, progress);
-          break;
-      }
-      return true;
-    });
-  }
-
-  @override
-  Stream<BluetoothDevice> get discovery => discoveryChannel
-          .receiveBroadcastStream(DateTime.now().millisecondsSinceEpoch)
-          .map(
-        (data) {
-          return BluetoothDevice(
-            address: data['address'],
-            name: data['name'] ?? '',
-            type: data['type'] ?? 1,
-          );
-        },
-      );
-
-  @override
-  Future<void> write({
-    required String address,
-    required Uint8List data,
-    ProgressCallback? onProgress,
-  }) async {
-    _init();
-
-    // ensure device is available
-    await discovery
-        .firstWhere((element) => element.address == address)
-        .timeout(const Duration(seconds: 10));
-
-    _progressCallback = onProgress;
-    await channel.invokeMethod('write', {
-      'address': address,
-      'data': data,
-    });
-    _progressCallback = null;
   }
 }
